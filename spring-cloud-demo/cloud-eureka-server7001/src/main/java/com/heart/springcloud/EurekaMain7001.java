62b4eb79039c99473a3c446726393bf14ea45e13
heart14 <lwf14@qq.com>
2021-01-06 11:42:29 +0800

    基础订单-支付模块中间加入单机版Eureka注册中心

--- spring-cloud-demo/cloud-eureka-server7001/src/main/java/com/heart/springcloud/EurekaMain7001.java
+++ spring-cloud-demo/cloud-eureka-server7001/src/main/java/com/heart/springcloud/EurekaMain7001.java
@@ -2,20 +2,20 @@ package com.heart.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * @ClassName: PaymentMain8001
+ * @ClassName: EurekaMain7001
  * @Description: TODO
  * @Author: jayhe
- * @Date: 2021/1/5 11:09
+ * @Date: 2021/1/6 10:42
  * @Version: v1.0
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
