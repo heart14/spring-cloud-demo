95334fe94000c3225573fa88af5fa0fc6e0b442e
heart14 <lwf14@qq.com>
2021-01-18 17:11:03 +0800

    基础订单-支付模块中间加入集群版Eureka注册并将微服务实例注册到Eureka集群中

---
+++ spring-cloud-demo/cloud-eureka-server7002/src/main/java/com/heart/springcloud/EurekaMain7002.java
@@ -0,0 +1,21 @@
+package com.heart.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName: EurekaMain7002
+ * @Description: TODO
+ * @Author: jayhe
+ * @Date: 2021/1/18 16:25
+ * @Version: v1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
