bafcace394de822681316b09b2c9e7d29d715671
heart14 <lwf14@qq.com>
2021-01-18 22:21:29 +0800

    服务发现DiscoveryClient

--- spring-cloud-demo/cloud-provider-payment8001/src/main/java/com/heart/springcloud/controller/PaymentController.java
+++ spring-cloud-demo/cloud-provider-payment8001/src/main/java/com/heart/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.heart.springcloud.entities.Payment;
 import com.heart.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @ClassName: PaymentController
@@ -48,6 +51,27 @@ public class PaymentController {
         } else {
             return new CommonResult(500, "Server Port[" + serverPort + "] FAIL！");
         }
+    }
+
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping(value = "/payment/discovery")
+    public CommonResult discovery() {
+        List<String> services = discoveryClient.getServices();
+        log.info("当前Eureka注册中心所有可提供的服务 :{}", services);
+
+        if (services != null) {
+            for (String service : services) {
+                List<ServiceInstance> instances = discoveryClient.getInstances(service);
+                for (ServiceInstance instance : instances) {
+                    log.info("[{}]服务的实例信息 :Host :{}, Port :{}, Uri :{}, InstanceId :{}, ServiceId :{}, Scheme :{}, Metadata :{}", service, instance.getHost(),instance.getPort(),instance.getUri(),instance.getInstanceId(),instance.getServiceId(),instance.getScheme(),instance.getMetadata());
+                }
+            }
+        }
+
+        return new CommonResult(200, "Server Port[" + serverPort + "] service discovery", discoveryClient);
     }
 
 }
