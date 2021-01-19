bbbf41711c502ce7b6d15044ac3e7a2c32df4b12
heart14 <lwf14@qq.com>
2021-01-19 17:12:24 +0800

    使用zookeeper注册中心

---
+++ spring-cloud-demo/cloud-provider-payment8004/src/main/java/com/heart/springcloud/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package com.heart.springcloud.controller;
+
+import com.heart.springcloud.entities.CommonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @ClassName: PaymentController
+ * @Description: TODO
+ * @Author: jayhe
+ * @Date: 2021/1/19 16:42
+ * @Version: v1.0
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public CommonResult paymentZk() {
+        return new CommonResult(200, "ZOOKEEPER service port [" + serverPort + "] SUCCESS! " + UUID.randomUUID().toString());
+    }
+
+}
