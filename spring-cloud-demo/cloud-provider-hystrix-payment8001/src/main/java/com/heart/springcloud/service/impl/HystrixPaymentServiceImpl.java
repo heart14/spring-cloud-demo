c3e47d5e415a08a59b609bbc1479310122fc177f
heart14 <lwf14@qq.com>
2021-02-03 16:28:02 +0800

    引入Hystrix，在服务提供者端进行服务降级处理

--- spring-cloud-demo/cloud-provider-hystrix-payment8001/src/main/java/com/heart/springcloud/service/impl/HystrixPaymentServiceImpl.java
+++ spring-cloud-demo/cloud-provider-hystrix-payment8001/src/main/java/com/heart/springcloud/service/impl/HystrixPaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.heart.springcloud.service.impl;
 
 import com.heart.springcloud.service.HystrixPaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -23,14 +25,38 @@ public class HystrixPaymentServiceImpl implements HystrixPaymentService {
         return "SERVICE SUCCESS! THREAD :" + Thread.currentThread().getName() + ", ID :" + id;
     }
 
+
+    /*
+     * @HystrixCommand注解为接口加入Hystrix服务降级处理
+     *      fallbackMethod指定当进行服务降级时，要调用的方法
+     *      commandProperties指定 超过3000毫秒没有做出响应，则进行服务降级
+     *
+     * 通过int a = 10 / 0;这行代码测试，程序运行异常时同样可以触发降级
+     */
+
+    /**
+     * 模拟程序处理超时
+     * @param id
+     * @return
+     */
     @Override
+    @HystrixCommand(fallbackMethod = "paymentInfo_timeoutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String paymentInfo_timeout(Integer id) {
         try {
-            //模拟程序处理耗时3秒才完成
+            //模拟程序处理耗时5秒才完成
             TimeUnit.SECONDS.sleep(5);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            log.error("{}",e.getMessage());
         }
-        return "SERVICE SUCCESS! THREAD :" + Thread.currentThread().getName() + ", ID :" + id;
+
+        //模拟程序运行报错
+        //int a = 10/0;
+        return "SERVICE SUCCESS!(*^_^*) THREAD :" + Thread.currentThread().getName() + ", ID :" + id;
+    }
+
+    public String paymentInfo_timeoutHandler(Integer id) {
+        return "SERVICE FAIL!(T_T) THREAD :" + Thread.currentThread().getName() + ", ID :" + id;
     }
 }
