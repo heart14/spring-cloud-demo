2fd11a3289ea13a7735f96cf87d1a2ba3e08d6b3
heart14 <lwf14@qq.com>
2021-02-03 17:49:49 +0800

    引入Hystrix，在服务消费者端controller层进行服务降级处理

--- spring-cloud-demo/cloud-consumer-feign-hystrix-order80/src/main/java/com/heart/springcloud/controller/OrderFeignHystrixController.java
+++ spring-cloud-demo/cloud-consumer-feign-hystrix-order80/src/main/java/com/heart/springcloud/controller/OrderFeignHystrixController.java
@@ -2,6 +2,8 @@ package com.heart.springcloud.controller;
 
 import com.heart.springcloud.entities.CommonResult;
 import com.heart.springcloud.service.FeignHystrixPaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,11 +33,41 @@ public class OrderFeignHystrixController {
         return commonResult;
     }
 
+//    @GetMapping(value = "/consumer/payment/hystrix/timeout/{id}")
+//    public CommonResult paymentInfo_timeout(@PathVariable("id") Integer id) {
+//        log.info("[CONSUMER] 通过FEIGN HYSTRIX调用PROVIDER => id :{}", id);
+//        CommonResult commonResult = feignHystrixPaymentService.paymentInfo_timeout(id);
+//        log.info("[CONSUMER] 通过OPEN FEIGN调用PROVIDER <= result :{}", commonResult);
+//        return commonResult;
+//    }
+
+
+    /**
+     * 在服务消费者端进行服务降级处理
+     *      指定超过2秒没有响应的时候触发降级
+     *      而此时服务提供者端配置的是 程序睡眠3秒，5秒没有响应触发提供者端的服务降级
+     *      所以通过这个接口测试的话，服务提供者端能够正常提供服务，而在消费者端由于Hystrix的配置，会触发服务降级
+     */
+
     @GetMapping(value = "/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentInfo_timeoutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5500")
+    })
     public CommonResult paymentInfo_timeout(@PathVariable("id") Integer id) {
         log.info("[CONSUMER] 通过FEIGN HYSTRIX调用PROVIDER => id :{}", id);
+
+        //调用远程服务自身超时时会进入fallback
         CommonResult commonResult = feignHystrixPaymentService.paymentInfo_timeout(id);
+
+        //同样 自身程序异常也会进入fallback
+        //int a = 10 / 0;
+
         log.info("[CONSUMER] 通过OPEN FEIGN调用PROVIDER <= result :{}", commonResult);
         return commonResult;
+    }
+
+    public CommonResult paymentInfo_timeoutHandler(@PathVariable("id") Integer id) {
+        log.error("[CONSUMER] 服务调用超时，进入FALLBACK METHOD( paymentInfo_timeoutHandler )");
+        return new CommonResult(500, "[CONSUMER] 服务调用失败，进入FALLBACK METHOD(paymentInfo_timeoutHandler). ID :" + id);
     }
 }
--- spring-cloud-demo/cloud-provider-hystrix-payment8001/src/main/java/com/heart/springcloud/service/impl/HystrixPaymentServiceImpl.java
+++ spring-cloud-demo/cloud-provider-hystrix-payment8001/src/main/java/com/heart/springcloud/service/impl/HystrixPaymentServiceImpl.java
@@ -27,6 +27,8 @@ public class HystrixPaymentServiceImpl implements HystrixPaymentService {
 
 
     /*
+     * 在服务提供者端进行服务降级处理
+     *
      * @HystrixCommand注解为接口加入Hystrix服务降级处理
      *      fallbackMethod指定当进行服务降级时，要调用的方法
      *      commandProperties指定 超过3000毫秒没有做出响应，则进行服务降级
@@ -39,24 +41,60 @@ public class HystrixPaymentServiceImpl implements HystrixPaymentService {
      * @param id
      * @return
      */
+//    @Override
+//    @HystrixCommand(fallbackMethod = "paymentInfo_timeoutHandler", commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+//    })
+//    public String paymentInfo_timeout(Integer id) {
+//        try {
+//            //模拟程序处理耗时5秒才完成
+//            TimeUnit.SECONDS.sleep(5);
+//        } catch (InterruptedException e) {
+//            log.error("{}",e.getMessage());
+//        }
+//
+//        //模拟程序运行报错
+//        //int a = 10/0;
+//        return "SERVICE SUCCESS!(*^_^*) THREAD :" + Thread.currentThread().getName() + ", ID :" + id;
+//    }
+
+
+    /**
+     * 服务降级fallback方法
+     * @param id
+     * @return
+     */
+    public String paymentInfo_timeoutHandler(Integer id) {
+        return "SERVICE FAIL!(T_T) THREAD :" + Thread.currentThread().getName() + ", ID :" + id;
+    }
+
+
+
+    /*
+     * 下面这部分用于测试在服务消费者端进行服务降级处理，保证提供者端能够正常响应而不是触发降级
+     *
+     * @HystrixCommand注解为接口加入Hystrix服务降级处理
+     *      fallbackMethod指定当进行服务降级时，要调用的方法
+     *      commandProperties指定 超过5000毫秒没有做出响应，则进行服务降级 而代码里设置线程只睡眠了3秒  所以能正常响应  而不是触发降级
+     *
+     */
+
+    /**
+     * 模拟程序处理超时
+     * @param id
+     * @return
+     */
     @Override
     @HystrixCommand(fallbackMethod = "paymentInfo_timeoutHandler", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
     })
     public String paymentInfo_timeout(Integer id) {
         try {
-            //模拟程序处理耗时5秒才完成
-            TimeUnit.SECONDS.sleep(5);
+            //模拟程序处理耗时3秒才完成
+            TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e) {
             log.error("{}",e.getMessage());
         }
-
-        //模拟程序运行报错
-        //int a = 10/0;
         return "SERVICE SUCCESS!(*^_^*) THREAD :" + Thread.currentThread().getName() + ", ID :" + id;
-    }
-
-    public String paymentInfo_timeoutHandler(Integer id) {
-        return "SERVICE FAIL!(T_T) THREAD :" + Thread.currentThread().getName() + ", ID :" + id;
     }
 }
