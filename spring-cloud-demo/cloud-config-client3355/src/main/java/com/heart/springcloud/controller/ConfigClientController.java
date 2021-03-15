f4a02ae2450c292ca8127edd813416a622aed3f4
heart14 <lwf14@qq.com>
2021-03-15 15:58:38 +0800

    SpringCloud Config客户端动态刷新配置信息

--- spring-cloud-demo/cloud-config-client3355/src/main/java/com/heart/springcloud/controller/ConfigClientController.java
+++ spring-cloud-demo/cloud-config-client3355/src/main/java/com/heart/springcloud/controller/ConfigClientController.java
@@ -2,6 +2,7 @@ package com.heart.springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")//这里的config.info是根据配置文件中实际的key值来写的
